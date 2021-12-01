73748c3abfd6497e597f955affcf83d211fe5cf4
300baud <300baudmodem@gmail.com>
2021-12-01 11:39:22 -0500

    Much nicer solution for Day 1, realized I needed to convert the vec of ints to a slice, which made `windows` and `filter` play nicely together. I wish the filter expression for part two was less unwieldy.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,27 @@
 use itertools::Itertools;
 
 pub fn day01_a() {
-    let readings = include_str!("input/day01")
+    let count = include_str!("input/day01")
         .lines()
-        .map(|line| line.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-
-    let mut prev: Option<u32> = None;
-    let mut count = 0u32;
-
-    for reading in readings {
-        if prev.is_some() && reading > prev.unwrap() {
-            count += 1;
-        }
-        prev = Some(reading)
-    }
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<u32>>()
+        .as_slice()
+        .windows(2)
+        .filter(|r| r[1] > r[0])
+        .count();
 
     println!("Part 01a saw {} measurements that were deeper", count);
 }
 
 pub fn day01_b() {
-    let readings = include_str!("input/day01")
+    let count = include_str!("input/day01")
         .lines()
         .map(|line| line.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-
-    let mut prev: Option<u32> = None;
-    let mut count = 0u32;
-
-    for triple in readings.windows(3) {
-        let reading = triple.into_iter().sum();
-        if prev.is_some() && reading > prev.unwrap() {
-            count += 1;
-        }
-        prev = Some(reading)
-    }
+        .collect::<Vec<u32>>()
+        .as_slice()
+        .windows(4)
+        .filter(|r| r[1..=3].iter().sum::<u32>() > r[0..=2].iter().sum::<u32>())
+        .count();
 
     println!("Part 01a saw {} measurements that were deeper", count);
 }
