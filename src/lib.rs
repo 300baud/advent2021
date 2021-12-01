b44cbba11d856e729feab14c43a9083b9c60f205
300baud <300baudmodem@gmail.com>
2021-12-01 12:01:34 -0500

    Added parallel execution of solutions and a stub for unsolved problems.

--- src/lib.rs
+++ src/lib.rs
@@ -23,5 +23,197 @@ pub fn day01_b() {
         .filter(|r| r[1..=3].iter().sum::<u32>() > r[0..=2].iter().sum::<u32>())
         .count();
 
-    println!("Part 01a saw {} measurements that were deeper", count);
+    println!("Part 01b saw {} measurements that were deeper", count);
+}
+
+pub fn day02_a() {
+    println!("day02_a not solved yet!");
+}
+
+pub fn day02_b() {
+    println!("day02_b not solved yet!");
+}
+
+pub fn day03_a() {
+    println!("day03_a not solved yet!");
+}
+
+pub fn day03_b() {
+    println!("day03_b not solved yet!");
+}
+
+pub fn day04_a() {
+    println!("day04_a not solved yet!");
+}
+
+pub fn day04_b() {
+    println!("day04_b not solved yet!");
+}
+
+pub fn day05_a() {
+    println!("day05_a not solved yet!");
+}
+
+pub fn day05_b() {
+    println!("day05_b not solved yet!");
+}
+
+pub fn day06_a() {
+    println!("day06_a not solved yet!");
+}
+
+pub fn day06_b() {
+    println!("day06_b not solved yet!");
+}
+
+pub fn day07_a() {
+    println!("day07_a not solved yet!");
+}
+
+pub fn day07_b() {
+    println!("day07_b not solved yet!");
+}
+
+pub fn day08_a() {
+    println!("day08_a not solved yet!");
+}
+
+pub fn day08_b() {
+    println!("day08_b not solved yet!");
+}
+
+pub fn day09_a() {
+    println!("day09_a not solved yet!");
+}
+
+pub fn day09_b() {
+    println!("day09_b not solved yet!");
+}
+
+pub fn day10_a() {
+    println!("day10_a not solved yet!");
+}
+
+pub fn day10_b() {
+    println!("day10_b not solved yet!");
+}
+
+pub fn day11_a() {
+    println!("day11_a not solved yet!");
+}
+
+pub fn day11_b() {
+    println!("day11_b not solved yet!");
+}
+
+pub fn day12_a() {
+    println!("day12_a not solved yet!");
+}
+
+pub fn day12_b() {
+    println!("day12_b not solved yet!");
+}
+
+pub fn day13_a() {
+    println!("day13_a not solved yet!");
+}
+
+pub fn day13_b() {
+    println!("day13_b not solved yet!");
+}
+
+pub fn day14_a() {
+    println!("day14_a not solved yet!");
+}
+
+pub fn day14_b() {
+    println!("day14_b not solved yet!");
+}
+
+pub fn day15_a() {
+    println!("day15_a not solved yet!");
+}
+
+pub fn day15_b() {
+    println!("day15_b not solved yet!");
+}
+
+pub fn day16_a() {
+    println!("day16_a not solved yet!");
+}
+
+pub fn day16_b() {
+    println!("day16_b not solved yet!");
+}
+
+pub fn day17_a() {
+    println!("day17_a not solved yet!");
+}
+
+pub fn day17_b() {
+    println!("day17_b not solved yet!");
+}
+
+pub fn day18_a() {
+    println!("day18_a not solved yet!");
+}
+
+pub fn day18_b() {
+    println!("day18_b not solved yet!");
+}
+
+pub fn day19_a() {
+    println!("day19_a not solved yet!");
+}
+
+pub fn day19_b() {
+    println!("day19_b not solved yet!");
+}
+
+pub fn day20_a() {
+    println!("day20_a not solved yet!");
+}
+
+pub fn day20_b() {
+    println!("day20_b not solved yet!");
+}
+
+pub fn day21_a() {
+    println!("day21_a not solved yet!");
+}
+
+pub fn day21_b() {
+    println!("day21_b not solved yet!");
+}
+
+pub fn day22_a() {
+    println!("day22_a not solved yet!");
+}
+
+pub fn day22_b() {
+    println!("day22_b not solved yet!");
+}
+
+pub fn day23_a() {
+    println!("day23_a not solved yet!");
+}
+
+pub fn day23_b() {
+    println!("day23_b not solved yet!");
+}
+
+pub fn day24_a() {
+    println!("day24_a not solved yet!");
+}
+
+pub fn day24_b() {
+    println!("day24_b not solved yet!");
+}
+
+pub fn day25_a() {
+    println!("day25_a not solved yet!");
+}
+
+pub fn day25_b() {
+    println!("day25_b not solved yet!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use advent2021::*;
+use rayon::prelude::*;
 
 fn main() {
-    day01_a();
-    day01_b();
+    rayon::ThreadPoolBuilder::new()
+        .stack_size(16_000_000)
+        .build_global()
+        .unwrap();
+
+    let funcs: &'static [fn()] = &[
+        day01_a, day01_b, day02_a, day02_b, day03_a, day03_b, day04_a, day04_b, day05_a, day05_b,
+        day06_a, day06_b, day07_a, day07_b, day08_a, day08_b, day09_a, day09_b, day10_a, day10_b,
+        day11_a, day11_b, day12_a, day12_b, day13_a, day13_b, day14_a, day14_b, day15_a, day15_b,
+        day16_a, day16_b, day17_a, day17_b, day18_a, day18_b, day19_a, day19_b, day20_a, day20_b,
+        day21_a, day21_b, day22_a, day22_b, day23_a, day23_b, day24_a, day24_b, day25_a, day25_b,
+    ];
+
+    (0..funcs.len()).into_par_iter().for_each(|i| funcs[i]());
 }
